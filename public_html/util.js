6094bed5c0b2062a18fc74de1ae4f8f0c42fe45c
John Quan <jquan2@alaska.edu>
2016-10-18 21:19:22 -0800

    Using util.js in navbar.html on Page 2

--- public_html/util.js
+++ public_html/util.js
@@ -9,23 +9,11 @@ else {
 	baseURL = "file://Users/johnpquan/UAF/Teaching/CS371_Computer_Ethics_and_Technical_Communication/HTML5Application/public_html";
 }
 
-function getPage(id, checkForAuthorization) {
+function getPage(id) {
 	var mainUrl = '';
-
-	if(checkForAuthorization) {
-		if(premium) {
-			mainUrl = mainUrl + id + '.html';
-		}
-		else {
-			mainUrl = mainUrl + 'unauthorized.html';
-		}
+            mainUrl = mainUrl + id + '.html';
 	}
-	else {
-		mainUrl = mainUrl + id + '.html';
-	}
-
 	$('#' + id).load(mainUrl);
-	resizeBorder();
 }
 
 function setHead(head) {
@@ -64,21 +52,6 @@ function resizeBorder() {
 	};
 }
 
-$(document).on("keypress", function(e) {
-	keytyped = String.fromCharCode(e.which);
-	console.log("Key pressed: " + keytyped);
-	e.preventDefault();
-
-	keystrokes = keystrokes + keytyped;
-	if(keystrokes.length > 9) {
-		keystrokes = keystrokes.substring(1,10);
-	}
-
-	if(keystrokes === "imawesome") {
-		alert("Woohoo! You're a premium member!");
-		premium = true;
-	}
-});
 
 window.onresize = function(event) {
 	resizeBorder();
@@ -107,25 +80,6 @@ function init() {
 	getPage('navigation');
 
 	resizeBorder();
-}
-
-function calculateContributions() {
-	var classNames = ['dustin','dylan','josh','scott'];
-	var elements;
-	var total = 0;
-
-	for(var j = 0; j < classNames.length; j++) {
-		elements = document.getElementsByClassName(classNames[j]);
-		for(var i = 0; i < elements.length; i++) {
-			var value = elements[i].innerHTML;
-			value = value.replace(/[%?]/gi, '');
-			if (value.length) total = total + parseInt(value);
-		}
-
-		totalElement = document.getElementById(classNames[j] + '-total');
-		totalElement.innerHTML = (total/elements.length + '%');
-		total = 0;
-	}
 }
 
 function moveToPart(position) {
