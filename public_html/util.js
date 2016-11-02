ee5442a72cd7484cb514d2c289f68993a3217733
John Quan <jquan2@alaska.edu>
2016-11-02 10:12:28 -0800

    Removed commented-out functions in util.js. home.html still inwork.

--- public_html/util.js
+++ public_html/util.js
@@ -1,102 +1,28 @@
-//var baseURL, mainURL;
-//var premium = false;
-//var keystrokes = "";
-//
-//if (document.location.href.indexOf('redmine') > -1) {
-//	baseURL = "https://projects.cs.uaf.edu/redmine/projects/cs371_group_projects/repository/revisions/master/raw/public_html/";
-//        mainURL = 'https://projects.cs.uaf.edu/redmine/projects/cs371_group_projects/wiki/';
-//}
-//else {
-//	baseURL = "file:///Users/johnpquan/UAF/Teaching/CS371_Computer_Ethics_and_Technical_Communication/HTML5Application/public_html/";
-//        mainURL = '';
-//}
-
+/**
+ * For a menu.html "onclick" event, get the html document matching this id
+ *  name, stop displaying its siblings, and only display this as a div.
+ * This is mainly used on index.html to only display the current div while 
+ * hiding sibling divs, but it can be used anywhere.
+ *  
+ * @param {type} id the div id, which should match the HTML page name
+ * @returns {undefined}
+ */
 function displaySingleDiv(id) {
-    //Stop displaying all other sibling divs on index.html
     $('#' + id).siblings().css("display", "none");
-    //When menu.html "onclick", get the div from it's page,
-    // and then load it into this div and display it.
     $.get(id + '.html', function (data) {
         $('#' + id).html(data).css("display", "block");
     });
 }
+/**
+ * Get the html document matching this id name and display as a div.
+ * This is mainly used on index.html to display menu, home, and footer_hfw, 
+ *  but it can be used anywhere.
+ *  
+ * @param {type} id the div id, which should match the HTML page name
+ * @returns {undefined}
+ */
 function displayDiv(id) {
     $.get(id + '.html', function (data) {
         $('#' + id).html(data).css("display", "block");
     });
 }
-//function setHead(head) {
-//	document.head.innerHTML = head;
-//}
-//
-//function getActiveTabPane() {
-//	var panes = document.getElementsByClassName('tab-pane');
-//
-//	for (var i = panes.length - 1; i >= 0; i--) {
-//		if (panes[i].className.indexOf('active') > -1) {
-//			return panes[i];
-//		};
-//	};
-//}
-//
-//function isWebsite() {
-//	if (document.location.href.indexOf('redmine') < 0)
-//		return true;
-//
-//	return false;
-//}
-//
-//function resizeBorder() {
-//	var borderDiv = $('.border');
-//	var newWidth = ((borderDiv.width() / 50) | 0);
-//
-//	borderDiv.css('border-width', newWidth + 'px');
-//	$("#whitepaper-iframe").width($(".whitepaper").width());
-//
-//	if (isWebsite()) {
-//		//If we're on the whitepaper page.
-//		if($('.documentation-menu > ul  > li.active').text() == "Whitepaper") {
-//			$('#whitepaper-iframe').height(675) //hard-coded hack for size
-//		}
-//	};
-//}
-//
-//
-//window.onresize = function(event) {
-//	resizeBorder();
-//};
-//
-//function resetBase() {
-//	var base = document.createElement('base');
-//	base.setAttribute('href', baseURL);
-//}
-//
-//function init() {
-//	document.write('');
-//	document.close();
-//	resetBase();
-//
-//	$.ajax({
-//	    url : (baseURL + "index.html"),
-//	    async:false,
-//	    success : function(result){
-//	    	$('html').html(result);
-//			// document.write(result);
-//		} 
-//	});
-//	
-//	resetBase();
-//	getPage('navigation');
-//
-//	resizeBorder();
-//}
-//
-//function moveToPart(position) {
-//	$('html,body').animate({
-//		scrollTop: $(position).offset().top
-//	}, 'slow');
-//}
-//
-//resetBase();	
-//resizeBorder();
-//getPage('index');
