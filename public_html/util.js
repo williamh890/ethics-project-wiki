1496046e772d7390d3a27dfb03e7eb2fac3d2b1e
John Quan <jquan2@alaska.edu>
2017-06-30 13:28:58 -0800

    Renamed my css file and menu.html.  Renamed my css id's and classes. Made many formatting and wording changes.

--- public_html/util.js
+++ public_html/util.js
@@ -1,5 +1,5 @@
 /**
- * For a menu.html "onclick" event, get the html document matching this id
+ * For a header.html "onclick" event, get the html document matching this id
  *  name, stop displaying its siblings, and only display this as a div.
  * This is mainly used on index.html to only display the current div while 
  * hiding sibling divs, but it can be used anywhere.
@@ -15,7 +15,7 @@ function displaySingleDiv(id) {
 }
 /**
  * Get the html document matching this id name and display as a div.
- * This is mainly used on index.html to display menu, home, and footer_hfw, 
+ * This is mainly used on index.html to display menu, home, and group-projects-footer, 
  *  but it can be used anywhere.
  *  
  * @param {type} id the div id, which should match the HTML page name
